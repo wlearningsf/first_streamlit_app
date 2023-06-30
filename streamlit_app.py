5f607a1442da39ed01a4a521bdea61de93b4efe8
wlearningsf <138257578+wlearningsf@users.noreply.github.com>
2023-06-30 15:39:43 -0600

    Adding new text to the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents new healthy dinner')
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
